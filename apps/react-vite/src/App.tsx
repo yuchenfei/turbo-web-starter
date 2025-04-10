e5321aca3f1110dc68dac304c893e7ddaa261a35
yuchenfei <cf.yu@qq.com>
2025-04-10 20:09:38 +0800

    chore: remove old Prettier configuration and update formatting

--- apps/react-vite/src/App.tsx
+++ apps/react-vite/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className='flex flex-col items-center justify-center min-h-svh'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       </div>
     </>
   )
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -14,8 +14,8 @@ const ThemeImage = (props: Props) => {
 
   return (
     <>
-      <Image {...rest} className="imgLight" src={srcLight} />
-      <Image {...rest} className="imgDark" src={srcDark} />
+      <Image {...rest} className='imgLight' src={srcLight} />
+      <Image {...rest} className='imgDark' src={srcDark} />
     </>
   )
 }
@@ -26,11 +26,11 @@ export default function Home() {
       <main className={styles['main']}>
         <ThemeImage
           className={styles['logo']}
-          srcDark="turborepo-light.svg"
-          srcLight="turborepo-dark.svg"
+          srcDark='turborepo-light.svg'
+          srcLight='turborepo-dark.svg'
           height={38}
           width={180}
-          alt="Turborepo logo"
+          alt='Turborepo logo'
           priority
         />
         <ol>
@@ -43,57 +43,57 @@ export default function Home() {
         <div className={styles['ctas']}>
           <a
             className={styles['primary']}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            rel="noopener noreferrer"
-            target="_blank"
+            href='https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo'
+            rel='noopener noreferrer'
+            target='_blank'
           >
             <Image
               className={styles['logo']}
-              src="/vercel.svg"
+              src='/vercel.svg'
               height={20}
               width={20}
-              alt="Vercel logomark"
+              alt='Vercel logomark'
             />
             Deploy now
           </a>
           <a
             className={styles['secondary']}
-            href="https://turbo.build/repo/docs?utm_source"
-            rel="noopener noreferrer"
-            target="_blank"
+            href='https://turbo.build/repo/docs?utm_source'
+            rel='noopener noreferrer'
+            target='_blank'
           >
             Read our docs
           </a>
         </div>
-        <Button className={styles['secondary']} appName="web">
+        <Button className={styles['secondary']} appName='web'>
           Open alert
         </Button>
       </main>
       <footer className={styles['footer']}>
         <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
+          href='https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          rel='noopener noreferrer'
+          target='_blank'
         >
           <Image
-            src="/window.svg"
+            src='/window.svg'
             height={16}
             width={16}
-            alt="Window icon"
+            alt='Window icon'
             aria-hidden
           />
           Examples
         </a>
         <a
-          href="https://turbo.build?utm_source=create-turbo"
-          rel="noopener noreferrer"
-          target="_blank"
+          href='https://turbo.build?utm_source=create-turbo'
+          rel='noopener noreferrer'
+          target='_blank'
         >
           <Image
-            src="/globe.svg"
+            src='/globe.svg'
             height={16}
             width={16}
-            alt="Globe icon"
+            alt='Globe icon'
             aria-hidden
           />
           Go to turbo.build →
